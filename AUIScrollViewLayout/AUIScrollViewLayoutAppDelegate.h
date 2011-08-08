c91376dbb57bbf515a4b1603b19355fda4e28e3f
Amos Elmaliah <amosel@gmail.com>
2011-08-08 18:56:16 -0400

    Initial Commit

---
+++ AUIScrollViewLayout/AUIScrollViewLayoutAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AUIScrollViewLayoutAppDelegate.h
+//  AUIScrollViewLayout
+//
+//  Created by Amos Elmaliah on 8/8/11.
+//  Copyright 2011 UIMUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AUIScrollViewLayoutViewController;
+
+@interface AUIScrollViewLayoutAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AUIScrollViewLayoutViewController *viewController;
+
+@end
---
+++ AUIScrollViewLayout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AUIScrollViewLayout
+//
+//  Created by Amos Elmaliah on 8/8/11.
+//  Copyright 2011 UIMUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AUIScrollViewLayoutAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AUIScrollViewLayoutAppDelegate class]));
+    }
+}
